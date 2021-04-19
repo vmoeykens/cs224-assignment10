d62bbacee83cc0c5a2311a3872e257dca81ce8b1
vmoeykens <vincent@beta.team>
2021-04-19 17:03:54 -0400

    Adds comments

--- Main.java
+++ Main.java
@@ -46,15 +46,25 @@ class Main {
 
 }
 
+/**
+ * Class to hold the data necessary to run the knapsack algorithm
+ */
 class Knapsack {
     int maxWeight; 
     ArrayList<Item> items;
     MemoizationTable memTab;
 
+    /**
+     * Create a knapsack object with the maxWeight initialized to -1 (this will be updated when a file is parsed)
+     */
     public Knapsack() {
         this.maxWeight = -1;
     }
 
+    /**
+     * Parse an input file and correctly set the maxWeight, and add items to the ArrayList. Also create a 
+     * memoization table initialized with the maxWeight and number of items
+     */
     public void parseFile(File fileName) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new FileReader(fileName)); 
         ArrayList<Item> out = new ArrayList<Item>();
@@ -74,6 +84,10 @@ class Knapsack {
         br.close();
     }
 
+    /**
+     * Run the knapsack algorithm, and display the state of the memoization table at each iteration. 
+     * At the end the memoization table will be completely updated.
+     */
     public void runAlgorithm() {
         System.out.println("\nSolving knapsack weight capacity " + this.getMaxWeight() + ", with " + Integer.toString(this.getItems().size() - 1) + " items");
         System.out.println("\n");
@@ -93,6 +107,10 @@ class Knapsack {
         }
     }
 
+    /**
+     * Compute the optimal set of items and the max value using
+     * the memoization table.
+     */
     public void computeOptimal() {
         ArrayList<Item> optimal = new ArrayList<Item>();
         int numItems = this.getItems().size() - 1;
@@ -116,6 +134,7 @@ class Knapsack {
         System.out.println("\nKnapsack contains: \n" + contentsString);
     }
 
+    // Getters and setters 
     public int getMaxWeight() {
         return this.maxWeight;
     }
@@ -131,20 +150,29 @@ class Knapsack {
     public void setItems(ArrayList<Item> items) {
         this.items = items;
     }
-
 }
 
+/**
+ * Class to represent an item in the knapsack
+ */
 class Item {
     int value;
     int weight;
     int index;
 
+    /**
+     * Constructor to create the item 
+     * @param value Value of the item
+     * @param weight Weight of the item
+     * @param index Index of the item in the knapsack list (this is kind of redundent and not used...)
+     */
     public Item(int value, int weight, int index) {
         this.value = value;
         this.weight = weight;
         this.index = index;
     }
 
+    // Getters
     public int getValue() {
         return this.value;
     }
@@ -164,9 +192,17 @@ class Item {
 
 }
 
+/**
+ * Class to represent the memoization table for running the knapack algorithm
+ */
 class MemoizationTable {
     ArrayList<ArrayList<MemItem>> table;
     
+    /**
+     * Constructor that initializes a table of all -1s
+     * @param maxWeight Max weight of the knapsack 
+     * @param numItems The number of items in the knapsack
+     */
     MemoizationTable(int maxWeight, int numItems) {
         this.table = new ArrayList<ArrayList<MemItem>>();
         for (int weight = 0; weight <= maxWeight; weight++) {
@@ -178,6 +214,7 @@ class MemoizationTable {
         }
     }
 
+    // Getter and setter
     void setItem(int weight, int item, MemItem value) {
         this.table.get(weight).set(item, value);
     }
@@ -210,11 +247,18 @@ class MemoizationTable {
 
 }
 
+/**
+ * Class to represent a memoization table item 
+ */
 class MemItem { 
     int val;
     Boolean include; 
 
-
+    /**
+     * Constructor
+     * @param val value of the item in the table
+     * @param include whether to consider for inclusion in the final solution
+     */
     public MemItem(int val, Boolean include) {
         this.val = val;
         this.include = include;
